ad673138e4ffee37e1d103356aa0a3a4ac9972cb
Benedykt Roszko <benroszko@gmail.com>
2020-03-21 14:38:23 +0100

    Add skiping comments

--- lab1/scanner.py
+++ lab1/scanner.py
@@ -3,27 +3,17 @@ from itertools import chain
 
 BINARY_OPERATORS = ['PLUS',  'MINUS',  'TIMES',  'DIVIDE']
 binary_literals = ['+', '-', '*', '/']
-# t_PLUS = r'\+'
-# t_MINUS = r'-'
-# t_TIMES = r'\*'
-# t_DIVIDE = r'/'
 
 
 MATRICES_OPERATORS = ['MPLUS',  'MMINUS',  'MTIMES',  'MDIVIDE']
-t_MPLUS = r'.+'
+t_MPLUS = r'.\+'
 t_MMINUS = r'.-'
-t_MTIMES = r'.*'
+t_MTIMES = r'.\*'
 t_MDIVIDE = r'./'
 
 
 BRACKETS = ['LBRACKET', 'RBRACKET', 'LSQUARE', 'RSQUARE', 'LBRACE', 'RBRACE']
 bracket_literals = ['(', ')', '[', ']', '{', '}']
-# t_LBRACKET = r'\('
-# t_BRACKET = r'\)'
-# t_LSQUARE = r'\['
-# t_rSQUARE = r'\]'
-# t_LBRACE = r'\{'
-# t_RBRACE = r'\}'
 
 
 RANGE_OPERATOR = ['RANGE']
@@ -38,17 +28,18 @@ COMMA_SEMICOLON_OPERATORS = ['COMMA', 'SEMICOLON']
 t_COMMA = r','
 t_SEMICOLON = r';'
 
+
 ASSIGN_OPERATORS = ['ASSIGN', 'ADD_ASSIGN',
                     'SUBSTRACT_ASSIGN', 'MULTIPLY_ASSIGN', 'DIVIDE_ASSIGN']
-RELATIONAL_OPERATORS = ['LESSER', 'GREATER', 'LESSER_EQUALS',
-                        'GREATER_EQUALS', 'NOT_EQUALS', 'EQUALS']
-
 t_ASSIGN = r'\='
-t_ADD_ADDIGN = r'\+='
+t_ADD_ASSIGN = r'\+='
 t_SUBSTRACT_ASSIGN = r'-='
 t_MULTIPLY_ASSIGN = r'\*='
 t_DIVIDE_ASSIGN = r'/='
 
+
+RELATIONAL_OPERATORS = ['LESSER', 'GREATER', 'LESSER_EQUALS',
+                        'GREATER_EQUALS', 'NOT_EQUALS', 'EQUALS']
 t_LESSER = r'<'
 t_GREATER = r'>'
 t_LESSER_EQUALS = r'<='
@@ -100,11 +91,31 @@ def t_STRING(t):
     return t
 
 
+COMMENT = ['COMMENT']
+
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+
 list_of_tokenlist = [BINARY_OPERATORS, MATRICES_OPERATORS, BRACKETS, RANGE_OPERATOR,
-                     TRANSPOSITION_OPERATOR, COMMA_SEMICOLON_OPERATORS, ASSIGN_OPERATORS, RELATIONAL_OPERATORS]
+                     TRANSPOSITION_OPERATOR, COMMA_SEMICOLON_OPERATORS, ASSIGN_OPERATORS, RELATIONAL_OPERATORS, FUNCTIONS, COMMENT]
 chain_object = chain.from_iterable(list_of_tokenlist)
-tokens = list(chain_object) + list(reserved.keys())
-t_ignore = '  \t\n'  # case => #
+tokens = list(chain_object) + list(reserved.values())
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+t_ignore = '  \t'  # case => #
 
 
 lexer = lex.lex()
