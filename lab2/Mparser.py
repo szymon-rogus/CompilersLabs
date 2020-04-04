ec5a7aebb7041351b65f42f20477f5169d0e8f32
Benedykt Roszko <benroszko@gmail.com>
2020-04-04 14:28:35 +0200

    Some progress

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -5,37 +5,66 @@ tokens = scanner.tokens
 
 precedence = (
     # to fill ...
-    ("left", '+', '-'),
+    ("right", 'ASSIGN'),
+    ("left", 'PLUS', 'MINUS'),
+    # ("left", '.+', '.-'),
+    ("left", 'TIMES', 'DIVIDE'),
+    # ("left", '.*', './'),
     # to fill ...
 )
 
+symbols_dict = {}
 
 def p_error(p):
     if p:
-        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, scanner.find_tok_column(p),
-                                                                                  p.type, p.value))
+        print("Syntax error at line %d: LexToken(%s, '%s')" % (p.lineno, p.type, p.value))
     else:
         print("Unexpected end of input")
 
 
 def p_program(p):
-    """program : instructions_opt"""
+    """PROGRAM : EXPRESSION"""
+    print("RESULT =", p[1])
 
 
-def p_instructions_opt_1(p):
-    """instructions_opt : instructions """
+def p_expression_number(p):
+    """EXPRESSION : INT
+                  | FLOAT"""
+    p[0] = p[1]
 
 
-def p_instructions_opt_2(p):
-    """instructions_opt : """
+def p_expression_id(p):
+    """EXPRESSION : ID"""
+    p[0] = p[1]
+    value = symbols_dict.get(p[1])
+    if value:
+        p[0] = value
+    else:
+        p[0] = 0
+        print("'%s' not used\n" % p[1])
 
 
-def p_instructions_1(p):
-    """instructions : instructions instruction """
+def p_expression_sum(p):
+    """EXPRESSION : EXPRESSION PLUS EXPRESSION
+                  | EXPRESSION MINUS EXPRESSION"""
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
 
 
-def p_instructions_2(p):
-    """instructions : instruction """
+def p_expression_assignment(p):
+    """EXPRESSION : EXPRESSION ASSIGN EXPRESSION"""
+    p[0] = p[3]
+    symbols_dict[p[1]] = p[3]
+
+
+# def p_instructions_1(p):
+#     """instructions : instructions instruction """
+#
+#
+# def p_instructions_2(p):
+#     """instructions : instruction """
 
 
 # to finish the grammar
