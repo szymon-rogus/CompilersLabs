744999599d664b683f70107aa821f61c054371ed
szymon-rogus <sszymek98@gmail.com>
2020-04-04 15:41:30 +0200

    relations

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -1,41 +1,45 @@
-import scanner
 import ply.yacc as yacc
+import scanner
 
 tokens = scanner.tokens
 
 precedence = (
     # to fill ...
-    ("left", '+', '-'),
+    ("right", 'ASSIGN'),
+    ("left", 'PLUS', 'MINUS'),
+    # ("left", '.+', '.-'),
+    ("left", 'TIMES', 'DIVIDE'),
+    # ("left", '.*', './'),
     # to fill ...
 )
+
+symbols_dict = {}
 
 
 def p_error(p):
     if p:
-        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, scanner.find_tok_column(p),
-                                                                                  p.type, p.value))
+        print("Syntax error at line %d: LexToken(%s, '%s')" % (p.lineno, p.type, p.value))
     else:
         print("Unexpected end of input")
 
-
-def p_program(p):
-    """program : instructions_opt"""
-
-
-def p_instructions_opt_1(p):
-    """instructions_opt : instructions """
-
-
-def p_instructions_opt_2(p):
-    """instructions_opt : """
+def p_expression_relational(p):
+    """ comparison_operator : LESS
+                            | MORE
+                            | EQUAL
+                            | INEQUAL
+                            | LESSEQUAL
+                            | MOREEQUAL"""
+    p[0] = p[1]
 
 
-def p_instructions_1(p):
-    """instructions : instructions instruction """
 
 
-def p_instructions_2(p):
-    """instructions : instruction """
+# def p_instructions_1(p):
+#     """instructions : instructions instruction """
+#
+#
+# def p_instructions_2(p):
+#     """instructions : instruction """
 
 
 # to finish the grammar
