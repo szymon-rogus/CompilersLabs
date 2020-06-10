d0f878bdaf8cf584ff28cd2449e2fe2dd4aa6c90
szymon-rogus <sszymek98@gmail.com>
2020-06-10 11:01:59 +0200

    lab5 prefiles

---
+++ lab5/Mparser.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python
+
+import ply.yacc as yacc
+import Ast as ast
+from scanner import lexer, tokens
+
+
+class Parser:
+    tokens = tokens
+
+    def __init__(self):
+        self.parser = yacc.yacc(module=self)
+        self.lexer = lexer
+        self.error = False
+
+    def parse(self, txt):
+        return self.parser.parse(txt, lexer=self.lexer)
+
+    precedence = (
+        ('nonassoc', 'IF'),
+        ('nonassoc', 'LESSER', 'GREATER', 'EQUALS', 'NOT_EQUALS', 'LESSER_EQUALS', 'GREATER_EQUALS', 'ELSE'),
+        ('left', 'PLUS', 'MINUS', 'MPLUS', 'MMINUS'),
+        ('left', 'TIMES', 'DIVIDE', 'MTIMES', 'MDIVIDE'),
+        ('left', 'UMINUS'),
+        ('nonassoc', 'TRANSPOSITION'),
+    )
+
+    def p_program(self, p):
+        """PROGRAM : CODE_BLOCK"""
+        p[0] = ast.Program(p[1])
+
+    def p_code_block_braces(self, p):
+        """
+        CODE_BLOCK : CODE_BLOCK LBRACE CODE_BLOCK RBRACE
+              | LBRACE CODE_BLOCK RBRACE
+        """
+        if len(p) == 5:
+            p[1].instructions.append(p[3])
+            p[0] = p[1]
+        else:
+            p[0] = ast.CodeBlock(p[2])
+
+    def p_code_block(self, p):
+        """
+        CODE_BLOCK : CODE_BLOCK instruction
+              | instruction
+        """
+        if len(p) == 3:
+            p[1].instructions.append(p[2])
+            p[0] = p[1]
+        else:
+            p[0] = ast.CodeBlock(p[1])
+
+    def p_instruction(self, p):
+        """
+        instruction : statement SEMICOLON
+                    | if_statement
+                    | while_statement
+                    | for_statement
+        """
+        p[0] = p[1]
+
+    def p_statement(self, p):
+        """
+        statement : assignment
+                  | keyword
+        """
+        p[0] = ast.Instruction(p[1])
+
+    def p_assignment(self, p):
+        """
+        assignment : variable assignment_operator expression
+        """
+        p[0] = ast.Assignment(p[1], p[2], p[3])
+
+    def p_variable(self, p):
+        """
+        variable : ID
+                 | variable_attribute
+        """
+        p[0] = ast.Variable(p[1])
+
+    def p_variable_attribute(self, p):
+        """
+        variable_attribute : ID LSQUARE sequence RSQUARE
+        """
+        p[0] = ast.VariableAttribute(p[1], p[3])
+
+    def p_sequence(self, p):
+        """
+        sequence : sequence COMMA expression
+                 | expression
+        """
+        if len(p) == 4:
+            p[1].expressions.append(p[3])
+            p[0] = p[1]
+        else:
+            p[0] = ast.Sequence(p[1])
+
+    def p_value(self, p):
+        """
+        value : FLOAT
+              | INT
+              | STRING
+              | matrix
+              | variable_attribute
+        """
+
+        p[0] = ast.Value(p[1])
+
+    def p_matrix(self, p):
+        """
+        matrix : LSQUARE rows RSQUARE
+        """
+        p[0] = ast.Matrix(p[2])
+
+    def p_rows(self, p):
+        """
+        rows : rows SEMICOLON sequence
+             | sequence
+        """
+        if len(p) == 2:
+            p[0] = ast.Rows(p[1])
+        else:
+            p[1].row_list.append(p[3])
+            p[0] = p[1]
+
+    def p_expression_value(self, p):
+        """
+        expression : value
+        """
+        p[0] = p[1]
+
+    def p_expression_id(self, p):
+        """expression : ID"""
+        p[0] = ast.Variable(p[1])
+
+    def p_expression_negation(self, p):
+        """
+        expression : MINUS expression %prec UMINUS
+        """
+        p[0] = ast.Negation(p[2])
+
+    def p_id_transposition(self, p):
+        """
+        expression : ID TRANSPOSITION
+        """
+        p[0] = ast.Transposition(ast.Variable(p[1]))
+
+    def p_expression_transposition(self, p):
+        """
+        expression : LBRACKET expression RBRACKET TRANSPOSITION
+        """
+        p[0] = ast.Transposition(p[2])
+
+    def p_expression_bracket(self, p):
+        """
+        expression : LBRACKET expression RBRACKET
+        """
+        p[0] = p[2]
+
+    def p_bin_expressions(self, p):
+        """
+        expression : expression PLUS expression
+                   | expression MINUS expression
+                   | expression TIMES expression
+                   | expression DIVIDE expression
+                   | expression MPLUS expression
+                   | expression MMINUS expression
+                   | expression MTIMES expression
+                   | expression MDIVIDE expression
+        """
+        p[0] = ast.BinaryExpression(p[1], p[2], p[3])
+
+    def p_expression_fun(self, p):
+        """
+        expression : function LBRACKET expression RBRACKET
+                   | function LBRACKET sequence RBRACKET
+        """
+        p[0] = ast.Function(p[1], p[3])
+
+    def p_keyword_print(self, p):
+        """
+        keyword : PRINT sequence
+        """
+        p[0] = ast.Print(p[2])
+
+    def p_keyword_break(self, p):
+        """
+        keyword : BREAK
+        """
+        p[0] = ast.Break()
+
+    def p_keyword_continue(self, p):
+        """
+        keyword : CONTINUE
+        """
+        p[0] = ast.Continue()
+
+    def p_keyword_return(self, p):
+        """
+        keyword : RETURN expression
+        """
+        p[0] = ast.Return(p[2])
+
+    def p_relation(self, p):
+        """relation : expression logic_operator expression"""
+        p[0] = ast.BinaryExpression(p[1], p[2], p[3])
+
+    def p_body(self, p):
+        """body : LBRACE CODE_BLOCK RBRACE
+                | instruction"""
+        if len(p) == 2:
+            p[0] = ast.Instruction(p[1])
+        else:
+            p[0] = ast.Instruction(p[2])
+
+    def p_if_statement(self, p):
+        """
+        if_statement : IF LBRACKET relation RBRACKET body %prec IF
+        """
+        p[0] = ast.If(p[3], p[5])
+
+    def p_if_else_statement(self, p):
+        """
+        if_statement : IF LBRACKET relation RBRACKET body ELSE body
+        """
+        p[0] = ast.If(p[3], p[5], p[7])
+
+    def p_while_statement(self, p):
+        """while_statement : WHILE LBRACKET relation RBRACKET body"""
+        p[0] = ast.While(p[3], p[5])
+
+    def p_for_statement(self, p):
+        """for_statement : FOR ID ASSIGN range body"""
+        p[0] = ast.For(p[2], p[4], p[5])
+
+    def p_range(self, p):
+        """range : expression RANGE expression RANGE expression
+                 | expression RANGE expression"""
+        if len(p) == 4:
+            p[0] = ast.Range(p[1], p[3])
+        else:
+            p[0] = ast.Range(p[1], p[3], p[5])
+
+    def p_assignment_operator(self, p):
+        """
+        assignment_operator : ASSIGN
+                            | ADD_ASSIGN
+                            | SUBSTRACT_ASSIGN
+                            | MULTIPLY_ASSIGN
+                            | DIVIDE_ASSIGN
+        """
+        p[0] = p[1]
+
+    def p_comparision_operator(self, p):
+        """
+        logic_operator : LESSER
+                       | GREATER
+                       | EQUALS
+                       | NOT_EQUALS
+                       | LESSER_EQUALS
+                       | GREATER_EQUALS
+        """
+        p[0] = p[1]
+
+    def p_function(self, p):
+        """
+        function : EYE
+                 | ZEROS
+                 | ONES
+        """
+        p[0] = p[1]
+
+    def p_error(self, p):
+        self.error = True
+        err_sign = "***********************"
+        if p:
+            line = p.lexer.lineno if hasattr(p.lexer, 'lineno') else p.lexer.lexer.lineno
+            value = p.value
+        else:
+            line = 'last'
+            value = ''
+        print('{}\nERROR\nIllegal symbol {} at line {}\n'.format(err_sign, value, line))
---
+++ lab5/scanner.py
@@ -0,0 +1,126 @@
+import ply.lex as lex
+from itertools import chain
+
+BINARY_OPERATORS = ['PLUS',  'MINUS',  'TIMES',  'DIVIDE']
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+
+
+MATRICES_OPERATORS = ['MPLUS',  'MMINUS',  'MTIMES',  'MDIVIDE']
+t_MPLUS = r'\.\+'
+t_MMINUS = r'\.-'
+t_MTIMES = r'\.\*'
+t_MDIVIDE = r'\./'
+
+
+BRACKETS = ['LBRACKET', 'RBRACKET', 'LSQUARE', 'RSQUARE', 'LBRACE', 'RBRACE']
+t_LBRACKET = r'\('
+t_RBRACKET = r'\)'
+t_LSQUARE = r'\['
+t_RSQUARE = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+
+
+RANGE_OPERATOR = ['RANGE']
+t_RANGE = r':'
+
+
+TRANSPOSITION_OPERATOR = ['TRANSPOSITION']
+t_TRANSPOSITION = r'\''
+
+
+COMMA_SEMICOLON_OPERATORS = ['COMMA', 'SEMICOLON']
+t_COMMA = r','
+t_SEMICOLON = r';'
+
+
+ASSIGN_OPERATORS = ['ASSIGN', 'ADD_ASSIGN',
+                    'SUBSTRACT_ASSIGN', 'MULTIPLY_ASSIGN', 'DIVIDE_ASSIGN']
+t_ASSIGN = r'\='
+t_ADD_ASSIGN = r'\+='
+t_SUBSTRACT_ASSIGN = r'-='
+t_MULTIPLY_ASSIGN = r'\*='
+t_DIVIDE_ASSIGN = r'/='
+
+
+RELATIONAL_OPERATORS = ['LESSER', 'GREATER', 'LESSER_EQUALS',
+                        'GREATER_EQUALS', 'NOT_EQUALS', 'EQUALS']
+t_LESSER = r'<'
+t_GREATER = r'>'
+t_LESSER_EQUALS = r'<='
+t_GREATER_EQUALS = r'>='
+t_NOT_EQUALS = r'!='
+t_EQUALS = r'\=='
+
+
+reserved = {
+    'if': 'IF',
+    'then': 'THEN',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT',
+}
+
+
+FUNCTIONS = ['ID', 'FLOAT', 'INT', 'STRING']
+
+
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+
+def t_FLOAT(t):
+    r'(\d*\.\d+E?\d*)|(\d+\.\d*E?\d*)'
+    t.value = float(t.value)
+    return t
+
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+t_STRING = r'\".*?\"'
+
+
+COMMENT = ['COMMENT']
+
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+
+list_of_tokenlist = [BINARY_OPERATORS, MATRICES_OPERATORS, BRACKETS, RANGE_OPERATOR,
+                     TRANSPOSITION_OPERATOR, COMMA_SEMICOLON_OPERATORS, ASSIGN_OPERATORS, RELATIONAL_OPERATORS, FUNCTIONS, COMMENT]
+chain_object = chain.from_iterable(list_of_tokenlist)
+tokens = list(chain_object) + list(reserved.values())
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+t_ignore = '  \t'
+
+
+lexer = lex.lex()
